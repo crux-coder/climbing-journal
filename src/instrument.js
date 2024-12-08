ce1727910fa15fe1cc99000e427ae7ab70ab4f89
Jasmin Mustafic <mustaficjasmin7@gmail.com>
2024-12-08 22:36:02 +0100

    Removed excess code.

--- src/instrument.js
+++ src/instrument.js
@@ -1,28 +1,16 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-import * as Sentry from "@sentry/node";
+const Sentry = require("@sentry/node");
 const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
   dsn: "https://23a30e5b529c0a2f3bf3666b5bade49e@o4508434425184256.ingest.de.sentry.io/4508434428919888",
   integrations: [nodeProfilingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Add Tracing by setting tracesSampleRate
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set sampling rate for profiling
+  // This is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
 });
-// Manually call startProfiler and stopProfiler
-// to profile the code in between
-console.log("Profiling started");
-Sentry.profiler.startProfiler();
-
-// Starts a transaction that will also be profiled
-Sentry.startSpan(
-  {
-    name: "My First Transaction",
-  },
-  () => {
-    // the code executing inside the transaction will be wrapped in a span and profiled
-  },
-);
-
-// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
-// your application until the process exits or stopProfiling is called.
-Sentry.profiler.stopProfiler();
